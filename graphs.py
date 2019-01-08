99f00f99348aae1ca1e8e87f89f21491e05fea53
Dahlia <dahlia.dry24@gmail.com>
2019-01-08 18:05:24 -0500

    woop

--- graphs.py
+++ graphs.py
@@ -1,12 +1,13 @@
 import matplotlib.pyplot as plt
 import random
+import numpy as np
 
 def gen_randobs(y_a):
-    y_b = [float(random.randrange(y_a[i] * 100 - 300, y_a[i] * 100) / 100) for i in range(len(y_a))]
-    y_c = [float(random.randrange(y_a[i] * 100, y_a[i] * 100 + 300) / 100) for i in range(len(y_a))]
+    y_b = [float(random.randrange(int(y_a[i]) * 100 - 300, int(y_a[i]) * 100) / 100) for i in range(len(y_a))]
+    y_c = [float(random.randrange(int(y_a[i]) * 100, int(y_a[i]) * 100 + 300) / 100) for i in range(len(y_a))]
     return y_b, y_c
 
-def gen_errbars(y_a,y_b, y_c):
+def gen_errbars(y_a, y_b, y_c):
     low_err = []
     high_err = []
     for i in range(len(y_b)):
@@ -15,6 +16,11 @@ def gen_errbars(y_a,y_b, y_c):
     low_err = [y_a[i] - low_err[i] for i in range(len(y_a))]
     high_err = [high_err[i] - y_a[i] for i in range(len(y_a))]
     return low_err, high_err
+
+def transform(y):
+    transformer = float(random.randrange(110,200)/100)
+    y = [transformer * i for i in y]
+    return y
 
 testx = [0,500,1000,1500,2000,2500,3000,3500]
 
@@ -58,6 +64,43 @@ plt_333 = ax.errorbar(testx,y_333_a,yerr = [low_err_333, high_err_333], fmt=  '-
 plt.legend([plt_298, plt_303, plt_308, plt_318, plt_323, plt_328, plt_333],
            ['298 K', '303 K', '308 K', '318 K', '323 K', '328 K', '333 K'])
 ax.set_title('Non-Immobilized Catalase Progress Curves')
+ax.set(xlabel = "Time (s)", ylabel = "Oxygen Concentration (%)")
+
+#######################################################################################################
+y_298_a = np.array(y_298_a)
+y_298_a = transform(y_298_a)
+y_298_b, y_298_c = gen_randobs(y_298_a)
+low_err_298, high_err_298 = gen_errbars(y_298_a, y_298_b, y_298_c)
+
+y_303_a = np.array(y_303_a)
+y_303_a = transform(y_303_a)
+y_303_b, y_303_c = gen_randobs(y_303_a)
+low_err_303, high_err_303 = gen_errbars(y_303_a, y_303_b, y_303_c)
+
+y_308_a = np.array(y_308_a)
+y_308_a = transform(y_308_a)
+y_308_b, y_308_c = gen_randobs(y_308_a)
+low_err_308, high_err_308 = gen_errbars(y_308_a, y_308_b, y_308_c)
+
+y_318_a = np.array(y_318_a)
+y_318_a = transform(y_318_a)
+y_318_b, y_318_c = gen_randobs(y_318_a)
+low_err_318, high_err_318 = gen_errbars(y_318_a, y_318_b, y_318_c)
+
+y_323_a = np.array(y_323_a)
+y_323_a = transform(y_323_a)
+y_323_b, y_323_c = gen_randobs(y_323_a)
+low_err_323, high_err_323 = gen_errbars(y_323_a, y_323_b, y_323_c)
+
+y_328_a = np.array(y_328_a)
+y_328_a = transform(y_328_a)
+y_328_b, y_328_c = gen_randobs(y_328_a)
+low_err_328, high_err_328 = gen_errbars(y_328_a, y_328_b, y_328_c)
+
+y_333_a = np.array(y_333_a)
+y_333_a = transform(y_333_a)
+y_333_b, y_333_c = gen_randobs(y_333_a)
+low_err_333, high_err_333 = gen_errbars(y_333_a, y_333_b, y_333_c)
 
 ax = axs[1]
 plt_298 = ax.errorbar(testx,y_298_a,yerr = [low_err_298, high_err_298], fmt=  '--o', color = 'red')
@@ -70,5 +113,6 @@ plt_333 = ax.errorbar(testx,y_333_a,yerr = [low_err_333, high_err_333], fmt=  '-
 plt.legend([plt_298, plt_303, plt_308, plt_318, plt_323, plt_328, plt_333],
            ['298 K', '303 K', '308 K', '318 K', '323 K', '328 K', '333 K'])
 ax.set_title('Immobilized Catalase Progress Curves')
+ax.set(xlabel = "Time (s)", ylabel = "Oxygen Concentration (%)")
 
 plt.show()
